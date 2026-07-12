23133e5a63a188c8b4ea74be39eb62025775105d
yacine <yacineaaddi@gmail.com>
2026-07-13 00:13:04 +0100

    Implement client-side cabin filtering (all, with discount, no discount)

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -7,6 +7,7 @@ import { useQuery } from "@tanstack/react-query";
 import useCabins from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 /* Replaced by compound component
 const Table = styled.div`
@@ -17,24 +18,23 @@ const Table = styled.div`
   overflow: hidden;
 `;*/
 
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
-
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+
+  if (filterValue === "all") filteredCabins = cabins;
+
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.disount === 0);
+
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.disount > 0);
 
   return (
     <Menus>
@@ -48,7 +48,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
+
+export default function CabinTableOperations() {
+  return (
+    <TableOperations>
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No-discount" },
+          { value: "with-discount", label: "With discount" },
+        ]}
+      />
+    </TableOperations>
+  );
+}
