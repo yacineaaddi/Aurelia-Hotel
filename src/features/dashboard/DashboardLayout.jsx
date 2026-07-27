f34f217b642c4211aac9a9256200f98b0bd993f3
yacine <yacineaaddi@gmail.com>
2026-07-27 22:56:38 +0100

    Building the Dashboard Layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import React from "react";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +7,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
