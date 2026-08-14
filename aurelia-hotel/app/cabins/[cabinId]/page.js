883d66017ae6d56aa3bbed3e7a34241cd5b160fb
yacine <yacineaaddi@gmail.com>
2026-08-14 21:42:03 +0100

    Make dynamic pages static with generateStaticParams

--- aurelia-hotel/app/cabins/[cabinId]/page.js
+++ aurelia-hotel/app/cabins/[cabinId]/page.js
@@ -1,10 +1,19 @@
 import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { getCabins } from "@/app/_lib/data-service";
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: cabin.id }));
+
+  return ids;
 }
 
 export default async function Page({ params }) {
