64f8e149ee0e412ae217fb870928345acb606878
yacine <yacineaaddi@gmail.com>
2026-08-18 17:24:59 +0100

    Creating a New Guest on First Sign In

--- aurelia-hotel/app/account/page.js
+++ aurelia-hotel/app/account/page.js
@@ -1,5 +1,12 @@
 export const metadata = { title: "Account" };
 
-export default function Page() {
-  return <h1>Hello Next!</h1>;
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {firstName}
+    </h2>
+  );
 }
